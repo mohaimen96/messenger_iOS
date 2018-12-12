79084008393444543ef235cd8e05adbb03f96f95
Mohaimen Hasan <mohaimen611@gmail.com>
2018-12-12 21:55:24 +0600

    Initial commit

---
+++ mymessenger/Model/Message.swift
@@ -0,0 +1,30 @@
+//
+//  Message.swift
+//  mymessenger
+//
+//  Created by Shaif Mohammad Reasat Dihan on 11/29/18.
+//  Copyright © 2018 Shaif Mohammad Reasat Dihan. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class Message: NSObject {
+    
+    var fromId: String?
+    var text: String?
+    var timestamp: NSNumber?
+    var toId: String?
+    
+    init(dictionary: [String: Any]) {
+        self.fromId = dictionary["fromId"] as? String
+        self.text = dictionary["text"] as? String
+        self.toId = dictionary["toId"] as? String
+        self.timestamp = dictionary["timestamp"] as? NSNumber
+    }
+    
+    func chatPartnerId() -> String? {
+        return fromId == Auth.auth().currentUser?.uid ? toId : fromId
+    }
+    
+}
---
+++ mymessenger/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  mymessenger
+//
+//  Created by Shaif Mohammad Reasat Dihan on 11/29/18.
+//  Copyright © 2018 Shaif Mohammad Reasat Dihan. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var id: String?
+    var name: String?
+    var email: String?
+    var profileImageUrl: String?
+    init(dictionary: [String: AnyObject]) {
+        self.id = dictionary["id"] as? String
+        self.name = dictionary["name"] as? String
+        self.email = dictionary["email"] as? String
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String
+    }
+}
